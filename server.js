d084f329bc63733bea09886a7eb537204cb75b1b
Jean-Baptiste de Borman <jb@noloading.com>
2013-12-02 11:51:48 +0100

    some loop

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World JB\n');
+  var string="dynamic string"; 
+  for (var n=0; n< 100; n++) {
+  string += n; 
+  	
+  }
+  res.end('Hello World\n' + string);
 }).listen(port);
